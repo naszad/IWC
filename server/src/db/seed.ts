b8a523e8e264392ae376fa0c62fde228bfc4ead2
Nathan Szadowski <nszadowski@butler.edu>
2025-03-17 22:23:15 -0400

    Revert "Add proficiency data seeder to database seeding process"
    This reverts commit 424b3ef86789badb54226fea92c6eda3d59006d7.

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,5 +1,4 @@
 import seedAssessments from './seeders/assessmentSeeder';
-import seedProficiencyData from './seeders/proficiencySeeder';
 
 async function seedDatabase() {
   try {
@@ -7,9 +6,6 @@ async function seedDatabase() {
     
     // Seed assessments
     await seedAssessments();
-    
-    // Seed proficiency data
-    await seedProficiencyData();
     
     // Add other seeders here if needed
     // e.g., await seedUsers();
